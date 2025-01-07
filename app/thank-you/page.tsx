92fc70e647892f480f28475fe661c10fced31c15
Ismail <Gaismail777@gmail.com>
2025-01-07 20:24:16 +0500

    Правки внесены и добавлена адаптивка для thank you page

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 const ThankYouPage = () => {
   return (
     <div className="w-full flex flex-col justify-center items-center h-screen">
-      <h1 className="text-[70px]">Tabriklaymiz!</h1>
-      <p className="text-[30px]">Muvaffaqiyatli ro‘yxatdan o‘tdingiz, siz bilan tez orada bog‘lanamiz</p>
+      <h1 className="md:text-[70px] text-[50px] text-center">Tabriklaymiz!</h1>
+      <p className="md:text-[30px] text-[20px] text-center">Muvaffaqiyatli ro‘yxatdan o‘tdingiz, siz bilan tez orada bog‘lanamiz</p>
     </div>
   );
 };

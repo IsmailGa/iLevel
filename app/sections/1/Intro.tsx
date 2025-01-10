5a98b82f0839389d5cb9d6ac0e5d3ad269213474
Ismail <Gaismail777@gmail.com>
2025-01-10 19:58:34 +0500

    Fixed styles

--- app/sections/1/Intro.tsx
+++ app/sections/1/Intro.tsx
@@ -17,7 +17,7 @@ const Intro = ({ setOpen }: SetOpenType) => {
       className="w-full flex flex-col items-center xl:pt-[175px] md:pt-[160px] xs:pt-[130px] pt-[18vw] relative justify-center"
       id="aboutus"
     >
-      <div className="absolute top-0 w-full z-[-1]">
+      <div className="absolute top-[2%] w-full z-[-1]">
         {/* Первое изображение: отображается на lg и больше */}
         <Image src={banner} alt="banner" className="hidden lg:block w-full" />
         {/* Второе изображение: отображается от sm до lg */}
@@ -35,20 +35,27 @@ const Intro = ({ setOpen }: SetOpenType) => {
       </div>
       {/* Основной блок */}
 
-      <div className="relative bg-primary w-full 2xl:h-[445px] xl:h-[400px] lg:h-[360px] md:h-[300px] h-[380px] rounded-[35px] p-[24px] md:p-[35px] flex flex-col items-center">
+      <div className="relative bg-primary w-full 2xl:h-[677px] xl:h-[677px] lg:h-[600px] md:h-[300px] h-[380px] rounded-[35px] p-[24px] md:p-[35px] flex flex-col items-center">
         {/* Фоновые линии */}
 
         {/* iPhone */}
-        <div className="absolute w-full z-[3] lg:w-[94%] md:w-[105%] md:top-[-18%] md:translate-y-[0] top-[50%] translate-y-[-50%] max-md:w-[114%]">
+        <div className="absolute w-full z-[3] lg:w-[94%] md:w-[105%] md:top-[25%] md:translate-y-[0] top-[50%] translate-y-[-50%] max-md:w-[114%]">
           <Image
             src={iPhone}
             alt="iPhone 16"
             className="w-full object-contain"
           />
         </div>
-        <div className="relative flex flex-col justify-end h-full w-full">
+        <div className="relative flex flex-col justify-between h-full w-full">
+          <p className="self-start text-white sm:text-[28px] sm:max-w-[755px] max-w-[350px] text-[18px] leading-[120%]">
+            <span className="text-[37px]">
+              Va nihoyat!
+            </span> <br /> <br />Endi siz boshlang&apos;ich 30% tolov va birgina
+            pasport bilan xalol muddatli to&apos;lovga iphone xarid qila
+            olasiz
+          </p>
           {/* Текст iPhone 16 */}
-          <div className="absolute md:bottom-[50%] bottom-[35%] translate-y-[60%] z-[2] flex items-center justify-center">
+          <div className="absolute md:bottom-[35%] bottom-[35%] translate-y-[60%] z-[2] flex items-center justify-center">
             <Image
               src={iPhone_text}
               alt="iPhone 16 Text"
@@ -57,26 +64,22 @@ const Intro = ({ setOpen }: SetOpenType) => {
           </div>
 
           {/* Текст и кнопка */}
-          <div className="relative z-[4] flex w-full h-full md:h-fit md:flex-row flex-col justify-between items-center">
-            {/* Текст */}
-            <p className="self-start md:self-center text-white sm:text-[20px] sm:max-w-[420px] max-w-[350px] text-[18px] leading-[120%]">
-              Va nihoyat! Endi siz boshlang&apos;ich 30% tolov va birgina
-              pasport bilan xalol muddatli to&apos;lovga iphone xarid qila
-              olasiz
+          {/* <div className="relative z-[4] flex w-full h-full md:h-fit md:flex-row flex-col justify-between items-center"> */}
+          {/* Текст */}
+
+          {/* Кнопка */}
+          <button
+            onClick={() => setOpen(true)}
+            className="self-end md:self-end flex items-center justify-between bg-white border-[1px] rounded-[25px] p-[3px] sm:h-[50px] h-[44px] group"
+          >
+            <p className="text-primary text-center px-[20px] text-[16px]">
+              Harid qilish
             </p>
-            {/* Кнопка */}
-            <button
-              onClick={() => setOpen(true)}
-              className="self-end md:self-center flex items-center justify-between bg-white border-[1px] rounded-[25px] p-[3px] sm:h-[50px] h-[44px] group"
-            >
-              <p className="text-primary text-center px-[20px] text-[16px]">
-                Harid qilish
-              </p>
-              <span className="flex items-center justify-center bg-primary rounded-[25px] p-[11px] shrink-0 aspect-square w-[auto] h-[100%] transition-transform duration-300 group-hover:rotate-[45deg]">
-                <Image src={arrow} alt="arrow_diagonal" />
-              </span>
-            </button>
-          </div>
+            <span className="flex items-center justify-center bg-primary rounded-[25px] p-[11px] shrink-0 aspect-square w-[auto] h-[100%] transition-transform duration-300 group-hover:rotate-[45deg]">
+              <Image src={arrow} alt="arrow_diagonal" />
+            </span>
+          </button>
+          {/* </div> */}
         </div>
       </div>
     </section>
